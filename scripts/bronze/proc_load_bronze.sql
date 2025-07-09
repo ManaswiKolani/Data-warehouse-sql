88a3e8b54bcc7e91c681434d4faf60bc21f16264
Manaswi Kolani <103065279+ManaswiKolani@users.noreply.github.com>
2025-07-09 08:17:53 -0500

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -39,7 +39,7 @@ BEGIN
 		SET @start_time = GETDATE();
 		TRUNCATE TABLE bronze.crm_cust_info
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\manas\OneDrive\Desktop\projects\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'path\to\datasets\source_crm\cust_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -54,7 +54,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.crm_prd_info;
         BULK INSERT bronze.crm_prd_info
-        FROM 'C:\Users\manas\OneDrive\Desktop\projects\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+        FROM 'path\to\datasets\source_crm\prd_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -69,7 +69,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.crm_sales_details;
         BULK INSERT bronze.crm_sales_details
-        FROM 'C:\Users\manas\OneDrive\Desktop\projects\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+        FROM 'path\to\datasets\source_crm\sales_details.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -91,7 +91,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.erp_cust_az12;
         BULK INSERT bronze.erp_cust_az12
-        FROM 'C:\Users\manas\OneDrive\Desktop\projects\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+        FROM 'path\to\datasets\source_erp\CUST_AZ12.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -106,7 +106,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.erp_loc_a101;
         BULK INSERT bronze.erp_loc_a101
-        FROM 'C:\Users\manas\OneDrive\Desktop\projects\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+        FROM 'path\to\datasets\source_erp\LOC_A101.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -121,7 +121,7 @@ BEGIN
         SET @start_time = GETDATE();
         TRUNCATE TABLE bronze.erp_px_cat_g1v2;
         BULK INSERT bronze.erp_px_cat_g1v2
-        FROM 'C:\Users\manas\OneDrive\Desktop\projects\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+        FROM 'path\to\datasets\source_erp\PX_CAT_G1V2.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
